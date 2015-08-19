e37778fcc43c7e731e42506a875bf0e9396cff18
cycorld <charleschoi87@gmail.com>
2015-08-19 15:12:08 +0900

    config.ru updated

--- app.rb
+++ app.rb
@@ -1,41 +1,42 @@
 require 'sinatra/base'
 require 'sinatra-websocket'
 
-class MyApp < Sinatra::Base
-set :server, 'thin'
-set :sockets, []
-set :rooms, {}
-enable :sessions
+class PlurryServer < Sinatra::Base
+  set :server, 'thin'
+  set :sockets, []
+  set :rooms, {}
+  enable :sessions
 
-get '/ws/:hash' do |hash|
-  if !request.websocket?
-    erb :index
-  else
-    request.websocket do |ws|
-      ws.onopen do
-        ws.send("Hello World! Channel : #{hash}")
-        settings.sockets << ws
-        settings.rooms[hash] = Array.new if settings.rooms[hash].class != Array
-        settings.rooms[hash] << ws.object_id
-        logger.info "======="
-        logger.info settings.inspect
-        logger.info settings.sockets.first.methods
-        logger.info settings.sockets.last.object_id
-        ws.send("object_id : " + ws.object_id.to_s)
-        session[:ws_id] = ws.object_id
-      end
-      ws.onmessage do |msg|
-        EM.next_tick do
-          settings.sockets.each do |s|
-            if settings.rooms[hash].include? (s.object_id)
-              s.send(msg)
+  get '/ws/:hash' do |hash|
+    if !request.websocket?
+      erb :index
+    else
+      request.websocket do |ws|
+        ws.onopen do
+          ws.send("Hello World! Channel : #{hash}")
+          settings.sockets << ws
+          settings.rooms[hash] = Array.new if settings.rooms[hash].class != Array
+          settings.rooms[hash] << ws.object_id
+          logger.info "======="
+          logger.info settings.inspect
+          logger.info settings.sockets.first.methods
+          logger.info settings.sockets.last.object_id
+          ws.send("object_id : " + ws.object_id.to_s)
+          session[:ws_id] = ws.object_id
+        end
+        ws.onmessage do |msg|
+          EM.next_tick do
+            settings.sockets.each do |s|
+              if settings.rooms[hash].include? (s.object_id)
+                s.send(msg)
+              end
             end
           end
         end
-      end
-      ws.onclose do
-        warn("websocket closed")
-        settings.sockets.delete(ws)
+        ws.onclose do
+          warn("websocket closed")
+          settings.sockets.delete(ws)
+        end
       end
     end
   end
