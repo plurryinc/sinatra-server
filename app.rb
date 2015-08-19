f50ba0b38a165467a05765ce76a11095622cb308
cycorld <charleschoi87@gmail.com>
2015-08-19 14:13:40 +0900

    websocket setted

---
+++ app.rb
@@ -0,0 +1,41 @@
+require 'sinatra'
+require 'sinatra-websocket'
+
+set :server, 'thin'
+set :sockets, []
+set :rooms, {}
+enable :sessions
+
+get '/ws/:hash' do |hash|
+  if !request.websocket?
+    erb :index
+  else
+    request.websocket do |ws|
+      ws.onopen do
+        ws.send("Hello World! #{hash}")
+        settings.sockets << ws
+        settings.rooms[hash] = Array.new if settings.rooms[hash].class != Array
+        settings.rooms[hash] << ws.object_id
+        logger.info "======="
+        logger.info settings.inspect
+        logger.info settings.sockets.first.methods
+        logger.info settings.sockets.last.object_id
+        ws.send(ws.object_id.to_s)
+        session[:ws_id] = ws.object_id
+      end
+      ws.onmessage do |msg|
+        EM.next_tick do
+          settings.sockets.each do |s|
+            if settings.rooms[hash].include? (s.object_id)
+              s.send(msg + session[:ws_id].to_s)
+            end
+          end
+        end
+      end
+      ws.onclose do
+        warn("websocket closed")
+        settings.sockets.delete(ws)
+      end
+    end
+  end
+end
