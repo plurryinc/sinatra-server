a4aa94f025254efed65f3902000d5b6e80ad9cb0
임광국 <kwangkuk0821@gmail.com>
2015-08-24 23:30:20 +0900

    Added user migration file

---
+++ db/migrate/20150824141615_create_users.rb
@@ -0,0 +1,9 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email,              null: false
+      t.string :encrypted_password, null: false
+      t.string :salt
+    end
+  end
+end
