62ab81a4136cc1d837cfa3a495acfda8c66dd532
임광국 <kwangkuk0821@gmail.com>
2015-08-25 14:52:21 +0900

    sign_in, sign_up function -> /users mapping

--- controllers/authentication_controller.rb
+++ controllers/authentication_controller.rb
@@ -4,17 +4,30 @@ class AuthenticationController < ApplicationController
     erb :sign_up
   end
 
-  post '/' do
+  post '' do
     sign_up = User.sign_up params
-    if sign_up.result == "success"
-      session[:user_id] = sign_up.data.id
+    if sign_up[:result] == "success"
+      session[:user_id] = sign_up[:data].id
+      redirect "/"
     else
+      redirect "/users/sign_in"
     end
-    redirect "/"
   end
 
   get '/sign_in' do
+    redirect "/" if session[:user_id]
     erb :sign_in
+  end
+
+  post '/sign_in' do
+    email = params[:email]
+    user = User.where(email: params[:email]).take
+
+    if user.encrypted_password == BCrypt::Engine.hash_secret(params[:password], user.salt)
+      session[:user_id] = user.id
+      redirect "/"
+    end
+    redirect "/users/sign_in"
   end
 
   post '/logout' do
