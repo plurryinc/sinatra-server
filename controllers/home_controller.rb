87d3295eeba6019bbf7278bf8af598b4e6c48521
임광국 <kwangkuk0821@gmail.com>
2015-08-26 12:09:40 +0900

    dashboard routing

--- controllers/home_controller.rb
+++ controllers/home_controller.rb
@@ -2,4 +2,8 @@ class HomeController < ApplicationController
   get '/' do
     erb :index, { :layout => :'layouts/main' }
   end
+
+  get '/dashboard' do
+    erb :dashboard, { :layout => :'layouts/dashboard' }
+  end
 end
