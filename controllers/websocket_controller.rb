4370b796ae93203460b3f499163a766347124a78
cycorld <charleschoi87@gmail.com>
2015-08-27 18:14:35 +0900

    seed update

--- controllers/websocket_controller.rb
+++ controllers/websocket_controller.rb
@@ -14,10 +14,6 @@ class WebsocketController < ApplicationController
           settings.sockets << ws
           settings.rooms[hash] = Array.new if settings.rooms[hash].class != Array
           settings.rooms[hash] << ws.object_id
-          logger.info "======="
-          logger.info settings.inspect
-          logger.info settings.sockets.first.methods
-          logger.info settings.sockets.last.object_id
           ws.send("object_id : " + ws.object_id.to_s)
           session[:ws_id] = ws.object_id
         end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,3 @@
-puts Dir.pwd
-Product.create(code: 'R2D2', product_id: 'feedbin-20150713-ffe0a3b2c', secret_token: 'q0w9e8r7', product_type: 1)
+user = User.sign_up(email: 'user@plurry.com', password: 'plurry', password_confirmation: 'plurry')
+group = Group.create(user_id: User.first.id, name: 'myrobot')
+product = Product.create(group_id: group.id, code: 'R2D2', product_id: 'feedbin-20150713-ffe0a3b2c', secret_token: 'q0w9e8r7', product_type: 1)
