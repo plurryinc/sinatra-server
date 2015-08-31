88218cfe4eae85d5832049c0088008a6ce6b7cf4
cycorld <charleschoi87@gmail.com>
2015-08-31 14:29:12 +0900

    websocket 튜닝

--- controllers/websocket_controller.rb
+++ controllers/websocket_controller.rb
@@ -1,4 +1,5 @@
 require 'sinatra-websocket'
+require 'json'
 
 class WebsocketController < ApplicationController
   set :sockets, []
@@ -21,7 +22,15 @@ class WebsocketController < ApplicationController
           EM.next_tick do
             settings.sockets.each do |s|
               if settings.rooms[hash].include? (s.object_id)
-                s.send(msg)
+                if is_valid_json? msg
+                  unless msg[:cmd].nil?
+                    s.send(msg)
+                  else
+                    # 서버 내부 처리 rs / report
+                  end
+                else
+                  # 무시 (로깅만)
+                end
               end
             end
           end
@@ -31,6 +40,55 @@ class WebsocketController < ApplicationController
           settings.sockets.delete(ws)
         end
       end
+    end
+  end
+
+  get '/debug/:hash' do |hash|
+    if !request.websocket?
+      erb :ws
+    else
+      request.websocket do |ws|
+        ws.onopen do
+          ws.send("Debug! Channel : #{hash}")
+          settings.sockets << ws
+          settings.rooms[hash] = Array.new if settings.rooms[hash].class != Array
+          settings.rooms[hash] << ws.object_id
+          ws.send("object_id : " + ws.object_id.to_s)
+          session[:ws_id] = ws.object_id
+        end
+        ws.onmessage do |msg|
+          EM.next_tick do
+            settings.sockets.each do |s|
+              if settings.rooms[hash].include? (s.object_id)
+                if is_valid_json? msg
+                  unless msg[:cmd].nil?
+                    s.send(msg)
+                  else
+                    # 서버 내부 처리 rs / report
+                  end
+                else
+                  # 무시 (로깅만)
+                end
+              end
+            end
+          end
+        end
+        ws.onclose do
+          warn("websocket closed")
+          settings.sockets.delete(ws)
+        end
+      end
+    end
+  end
+
+  private
+
+  def is_valid_json? string
+    begin
+      JSON.parse string
+      return true
+    rescue Exception => e
+      return false
     end
   end
 end
