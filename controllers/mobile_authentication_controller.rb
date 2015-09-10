6af3e42d2f0c9500e7cca84f0133eccc523ee36e
임광국 <kwangkuk0821@gmail.com>
2015-09-10 15:07:20 +0900

    sign_up secret token

--- controllers/mobile_authentication_controller.rb
+++ controllers/mobile_authentication_controller.rb
@@ -13,12 +13,13 @@ class MobileAuthenticationController < ApplicationController
 
     if !user.nil? and user.encrypted_password == BCrypt::Engine.hash_secret(params[:password], user.salt)
       unless user.mobile_secret_token.nil?
-        session[:mobile_secret_token] = user.mobile_secret_token
-        return { result: "success", what: "sign_in", cache_token: user.mobile_secret_token }.to_json
+        authenticate_token! user.mobile_secret_token
+        return { result: "success", what: "sign_in", secret_token: user.mobile_secret_token }.to_json
       else
         secret_token = Digest::SHA1.hexdigest Time.now.to_f.to_s
         user.update(mobile_secret_token: secret_token)
-        return { result: "success", what: "sign_in", cache_token: secret_token }.to_json
+        authenticate_token! secret_token
+        return { result: "success", what: "sign_in", secret_token: secret_token }.to_json
       end
     else
       return { result: "fail", what: "sign_in" }.to_json
@@ -28,7 +29,14 @@ class MobileAuthenticationController < ApplicationController
   post '/sign_up' do
     warn(params)
     sign_up = User.sign_up params
-    return { result: sign_up[:result], what: "sign_up" }
+    if(sign_up[:result] == "success")
+      secret_token = Digest::SHA1.hexdigest Time.now.to_f.to_s
+      sign_up[:data].update(mobile_secret_token: secret_token)
+      authenticate_token! secret_token
+      return { result: sign_up[:result], what: "sign_up", secret_token: secret_token }.to_json
+    else
+      return { result: sign_up[:result], what: "sign_up" }.to_json
+    end
   end
 
   post '/sign_out' do
--- controllers/mobile_main_controller.rb
+++ controllers/mobile_main_controller.rb
@@ -1,10 +1,11 @@
 class MobileMainController < ApplicationController
   before do
     content_type :json
-    unless m_user_signed_in?
-      unless params[:secret_token].nil?
-        authenticate_token! params[:secret_token]
-      else
+    if(!m_user_signed_in? && params[:secret_token].nil?)
+      result = { result: "fail", what: "authenticate_token" }.to_json
+      halt 401, {'Content-Type' => 'text/plain'}, result
+    else
+      unless m_current_user.mobile_secret_token == params[:secret_token]
         result = { result: "fail", what: "authenticate_token" }.to_json
         halt 401, {'Content-Type' => 'text/plain'}, result
       end
