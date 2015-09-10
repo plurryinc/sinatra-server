3d62e938aaaa406d77b66efa80d7b599895f7800
임광국 <kwangkuk0821@gmail.com>
2015-09-10 14:15:35 +0900

    content_type json

--- controllers/mobile_authentication_controller.rb
+++ controllers/mobile_authentication_controller.rb
@@ -1,5 +1,9 @@
 class MobileAuthenticationController < ApplicationController
-  get '/sign_in' do
+  before do
+    content_type :json
+  end
+
+  post '/sign_in' do
     email = params[:email]
     password = params[:password]
 
@@ -10,14 +14,14 @@ class MobileAuthenticationController < ApplicationController
     if !user.nil? and user.encrypted_password == BCrypt::Engine.hash_secret(params[:password], user.salt)
       unless user.mobile_secret_token.nil?
         session[:mobile_secret_token] = user.mobile_secret_token
-        return JSON.parse({ result: "success", what: "sign_in", cache_token: user.mobile_secret_token }.to_json)
+        return { result: "success", what: "sign_in", cache_token: user.mobile_secret_token }.to_json
       else
         secret_token = Digest::SHA1.hexdigest Time.now.to_f.to_s
         user.update(mobile_secret_token: secret_token)
-        return JSON.parse({ result: "success", what: "sign_in", cache_token: secret_token }.to_json)
+        return { result: "success", what: "sign_in", cache_token: secret_token }.to_json
       end
     else
-      return { result: "fail", what: "sign_in" }.to_s.to_json
+      return { result: "fail", what: "sign_in" }.to_json
     end
   end
 
@@ -31,9 +35,9 @@ class MobileAuthenticationController < ApplicationController
     unless session[:mobile_secret_token].nil?
       m_current_user.update(mobile_secret_token: nil)
       session[:mobile_secret_token] = nil
-      return JSON.parse({ result: "success", what: "logout" }.to_json)
+      return { result: "success", what: "logout" }.to_json
     else
-      return JSON.parse({ result: "fail", what: "logout" }.to_json)
+      return { result: "fail", what: "logout" }.to_json
     end
   end
 end
--- controllers/mobile_main_controller.rb
+++ controllers/mobile_main_controller.rb
@@ -1,10 +1,11 @@
 class MobileMainController < ApplicationController
   before do
+    content_type :json
     unless m_user_signed_in?
       unless params[:secret_token].nil?
         authenticate_token! params[:secret_token]
       else
-        result = JSON.parse({ result: "fail", what: "authenticate_token" }.to_json)
+        result = { result: "fail", what: "authenticate_token" }.to_json
         halt 401, {'Content-Type' => 'text/plain'}, result
       end
     end
