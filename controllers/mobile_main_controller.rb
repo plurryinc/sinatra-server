1bfa2c610395c21a8f58038959dda5956d32d701
임광국 <kwangkuk0821@gmail.com>
2015-09-11 15:40:24 +0900

    mobile before filter modify

--- controllers/mobile_main_controller.rb
+++ controllers/mobile_main_controller.rb
@@ -1,14 +1,11 @@
 class MobileMainController < ApplicationController
   before do
     content_type :json
-    if(!m_user_signed_in? && params[:secret_token].nil?)
+    unless params[:secret_token].nil? && User.where(mobile_secret_token: params[:secret_token]).empty?
+      authenticate_token! params[:secret_token]
+    else
       result = { result: "fail", what: "authenticate_token" }.to_json
       halt 401, {'Content-Type' => 'text/plain'}, result
-    else
-      unless m_current_user.mobile_secret_token == params[:secret_token]
-        result = { result: "fail", what: "authenticate_token" }.to_json
-        halt 401, {'Content-Type' => 'text/plain'}, result
-      end
     end
   end
 
