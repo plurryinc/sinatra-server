548a48936d421c5955d4f7c954a8b1c2fbbdb0a6
임광국 <kwangkuk0821@gmail.com>
2015-09-21 11:29:53 +0900

    schedule default value

--- models/product.rb
+++ models/product.rb
@@ -1,6 +1,22 @@
 class Product < ActiveRecord::Base
+  after_initialize :schedule_create
   belongs_to :group
   has_many :logs
 
   serialize :schedule, Array
+
+  def schedule_create
+    if self.product_type == 1
+      default = []
+      0.upto(19) do |i|
+        default.push({
+          id: i,
+          status: true,
+          time: "empty",
+          amount: 0
+        })
+      end
+      self.update(schedule: default)
+    end
+  end
 end
