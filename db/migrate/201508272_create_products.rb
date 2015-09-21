9f6c22297b28a47a081a8d59bcb94dd6d2d31958
임광국 <kwangkuk0821@gmail.com>
2015-09-21 16:35:00 +0900

    schedule default values

--- db/migrate/201508272_create_products.rb
+++ db/migrate/201508272_create_products.rb
@@ -2,13 +2,22 @@ require 'sinatra/activerecord'
 
 class CreateProducts < ActiveRecord::Migration
   def change
+    default = []
+    0.upto(19) do |i|
+      default.push({
+        id: i,
+        status: true,
+        time: "empty",
+        amount: 0
+      }.stringify_keys!)
+    end
     create_table :products do |t|
       t.integer :group_id
       t.string  :code,          null: false
       t.string  :product_id,    null: false
       t.string  :secret_token,  null: false
       t.integer :product_type,  null: false
-      t.text    :schedule
+      t.text    :schedule, default: default
 
       t.timestamps null: false
     end
--- models/product.rb
+++ models/product.rb
@@ -1,22 +1,6 @@
 class Product < ActiveRecord::Base
-  after_initialize :schedule_create
   belongs_to :group
   has_many :logs
 
   serialize :schedule, Array
-
-  def schedule_create
-    if self.product_type == 1 && self.schedule.empty?
-      default = []
-      0.upto(19) do |i|
-        default.push({
-          id: i,
-          status: true,
-          time: "empty",
-          amount: 0
-        }.stringify_keys!)
-      end
-      self.update(schedule: default)
-    end
-  end
 end
